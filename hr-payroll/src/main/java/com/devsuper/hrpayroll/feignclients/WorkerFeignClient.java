81c0e6777d099f23025d31cc213cf9fa169fbfdd
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-13 17:35:09 -0300

    Feign client

---
+++ hr-payroll/src/main/java/com/devsuper/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,22 @@
+package com.devsuper.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.devsuper.hrpayroll.entities.Worker;
+
+
+@Component
+@FeignClient(
+    name = "hr-worker",
+    url = "http://localhost:8001",
+    path = "/workers"
+)
+public interface WorkerFeignClient {
+
+    @GetMapping("/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
