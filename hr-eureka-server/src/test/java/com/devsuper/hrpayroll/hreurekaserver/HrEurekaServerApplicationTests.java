3090f854f56e021324b4aff4561f8d361e47851d
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-15 14:51:34 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/devsuper/hrpayroll/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuper.hrpayroll.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
