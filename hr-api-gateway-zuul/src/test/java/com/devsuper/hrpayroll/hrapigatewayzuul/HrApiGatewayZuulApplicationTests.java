bcdb4f71cc6f6bb94ac4b70768b7a5ffb1afac9d
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-15 16:19:25 -0300

    Add hr-api-gateway-zuul microservice

---
+++ hr-api-gateway-zuul/src/test/java/com/devsuper/hrpayroll/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuper.hrpayroll.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
