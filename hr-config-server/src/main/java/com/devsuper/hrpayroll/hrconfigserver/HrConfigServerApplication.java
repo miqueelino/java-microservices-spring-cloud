d1543d34b057a0f6516c6efa10222ac576f6f201
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-19 15:38:41 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/devsuper/hrpayroll/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuper.hrpayroll.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
