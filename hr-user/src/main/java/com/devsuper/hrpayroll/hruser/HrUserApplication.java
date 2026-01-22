33f3eb16ff8a5d424729f64318004e070d67dfb7
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-22 14:47:19 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/devsuper/hrpayroll/hruser/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.devsuper.hrpayroll.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/com/devsuper/hrpayroll/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuper.hrpayroll.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
