ea3b27474d3bafd74256773f1248d860cb6d6a00
miqueelino <joao.jnunes@mercadolivre.com>
2026-02-02 17:02:29 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/devsuper/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuper.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
