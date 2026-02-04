a33d6e5baf7939e0763f9b6513e34c83bae1ef01
miqueelino <joao.jnunes@mercadolivre.com>
2026-02-04 16:21:50 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuper/hrpayroll/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.devsuper.hrpayroll.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MySecretKey");
+		return tokenConverter;
+	}
+	
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+}
